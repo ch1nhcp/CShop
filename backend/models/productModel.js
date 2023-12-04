cd27bbfb9c5f0820f3f55f8ce119d193bbc8f371
chinhcp <chinhcpdev@gmail.com>
2023-12-04 10:14:29 +0700

    add seeding sample data

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -3,25 +3,25 @@ import mongoose from "mongoose";
 const reviewSchema = mongoose.Schema(
   {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User",
-      },
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     name: {
       type: String,
       required: true,
     },
     rating: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     comment: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-},
-{ timestamps: true }
-)};
+  },
+  { timestamps: true }
+);
 
 const productSchema = mongoose.Schema(
   {
