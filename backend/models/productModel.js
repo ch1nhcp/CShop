ab4e508f9bcfc17341a7678d5e57f58ff2029107
chinhcp <chinhcpdev@gmail.com>
2023-12-04 10:11:09 +0700

    hot fix model

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const reviewSchema = {reviewSchema = new mongoose.Schema(
+const reviewSchema = mongoose.Schema(
   {
     user: {
         type: mongoose.Schema.Types.ObjectId,
@@ -23,7 +23,7 @@ const reviewSchema = {reviewSchema = new mongoose.Schema(
 { timestamps: true }
 )};
 
-const productSchema = new mongoose.Schema(
+const productSchema = mongoose.Schema(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
